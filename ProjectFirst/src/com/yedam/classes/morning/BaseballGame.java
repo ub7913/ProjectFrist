fce677ca7997b363366e7e2929182cebca06eb29
ub7913 <64175752+ub7913@users.noreply.github.com>
2020-06-01 07:31:43 +0900

    home

--- ProjectFirst/src/com/yedam/classes/morning/BaseballGame.java
+++ ProjectFirst/src/com/yedam/classes/morning/BaseballGame.java
@@ -10,19 +10,46 @@ public class BaseballGame {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		System.out.println("야구게임을 시작합니다");
-		//int[] selectNum = new int[3];
+		// int[] selectNum = new int[3];
 		List<Integer> selectNum = new ArrayList<>();
+		List<Integer> selectNum2 = new ArrayList<>();
 		Random random = new Random();
 
-		for (int i=0; i<3; i++) {
-			for (int j=0; j<3; j++) {
-				if(selectNum.get(i) != selectNum.get(j))
-			}
-			if(
+		while (true) {
+			for (int i = 0; i < 3; i++) {
 				selectNum.add(random.nextInt(9)+1);
+			}
+			if (selectNum.get(0)!=selectNum.get(1)&&selectNum.get(1)!=selectNum.get(2)&&selectNum.get(0)!=selectNum.get(2)) {
+				break;
+			}
 		}
+		System.out.println(selectNum);
 		
-		
+		while (true) {
+			System.out.println("번호를 입력하세요");
+			int strike=0;
+			int ball=0;
+			for (int i =0; i<3; i++) {
+				int num=scn.nextInt();
+				selectNum2.add(num);
+			}
+			for (Integer i=0; i<3; i++) {
+				for (Integer j=0; j<3; j++) {
+					if (selectNum.get(i).equals(selectNum2.get(j))&&i==j) {
+						strike++;
+					} else if (selectNum.get(i).equals(selectNum2.get(j))&&i!=j) {
+						ball++;
+					}
+				}
+			}
+			System.out.println(strike + "스트라이크" + ball + "볼 입니다.");
+			if (strike==3) {
+				break;
+			}
+		}
+	}
+}
+
 //		while (true) {
 //			Integer a = random.nextInt(9)+1;
 //			selectNum.add(a);
@@ -33,9 +60,8 @@ public class BaseballGame {
 //					break;
 //				}
 //			
-				
-				
-			//}
+
+// }
 //		for (int i = 0; i < 3; i++) {
 //			Integer a = random.nextInt(9)+1;
 //			for(int j=0; j<3; j++) {
@@ -44,11 +70,10 @@ public class BaseballGame {
 //				}
 //				System.out.println(selectNum);
 //			}
-			
-			
+
 //			selectNum[i] = random.nextInt(9) + 1;
-	//	}
-		//System.out.print(selectNum + " ");
+// }
+// System.out.print(selectNum + " ");
 //		System.out.println();
 //		System.out.println("번호를 선택하시오");
 //		while (true) {
@@ -81,7 +106,7 @@ public class BaseballGame {
 //				System.out.println("삼진아웃!! 게임을 종료합니다");
 //				break;
 //			}
-			//break;
-	//	} 
-	}
-}
+// break;
+// }
+//	}
+//}
